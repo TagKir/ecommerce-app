6bad5f9c2c4d71e23354077cfd72aeee6095e1c5
TagKir <tagirkiky@gmail.com>
2024-08-07 20:51:02 +0300

    Поскачивал модули и добавил основу для swagger'а

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-var cors = require("cors");
-
-const app = express();
-const port = 3000;
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const cors = require("cors");
 
 const users = require("./api/users");
 const orders = require("./api/orders");
 const products = require("./api/products");
 const carts = require("./api/carts");
 
+const app = express();
+const port = 3000;
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
+
+var swaggerDefinition = {
+  info: {
+    title: "Node Swagger API",
+    version: "1.0.0",
+    description: "Demonstrating how to describe a RESTful API with Swagger",
+  },
+  host: "localhost:3000",
+  basePath: "/",
+};
+
+var options = {
+  swaggerDefinition: swaggerDefinition,
+
+  apis: ["./api/*.js"],
+};
+
+var swaggerSpec = swaggerJSDoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use("/users", users);
 app.use("/orders", orders);
